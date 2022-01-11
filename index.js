8fbb06be69eb7015dac03459559da306ffae4d4a
alexandra <alexandraschurman@gmail.com>
2022-01-10 16:02:57 -0800

    commit tree e98042907d5cadde36f265f57832fa76bf7a209c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply
+    const num1 = 31
+    const num2 = 2
+    multiply = num1 * num2
 
-// Write your code here
+  
+   
+ let random = Math.floor(Math.random() * 10) + 1;
+
+
+let mod
+    const num3 = 8
+    const num4 = 2
+    mod = num3 / num4
+
+    let max = Math.max(5, 10, 15, 20)
